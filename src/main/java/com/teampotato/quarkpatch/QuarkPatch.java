b905ce63d325e2d5da64367ebbdfa1ebbec024af
Kasualix <670067575@qq.com>
2023-06-27 19:07:55 +0800

    Initial commit

---
+++ src/main/java/com/teampotato/quarkpatch/QuarkPatch.java
@@ -0,0 +1,8 @@
+package com.teampotato.quarkpatch;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(QuarkPatch.ID)
+public class QuarkPatch {
+    public static final String ID = "quarkpatch";
+}
